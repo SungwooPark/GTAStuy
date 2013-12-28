069d452239d74a49a0e81837e04aa1a1b2a553ac
albertoh61 <albertodoh61@gmail.com>
2013-12-28 12:35:00 -0500

    updated

--- actual/Character.java
+++ actual/Character.java
@@ -3,4 +3,14 @@ import java.io.*;
 
 public class Character {
 	private int health;
-	private string name;
+
+	public Character (String name){
+		health = 50;
+		
+	}
+		
+
+	public String intro () {
+		String intro = "Welcome back " + name + ", we hope you enjoyed your summer, but now it is time to get back to school. You have just crossed the Tribeca and face the haunting metal doors that are the entrance to Stuy. You enter and encounter a bright flash of light........./n/n" + "          || Welcome to GTAstuy ||";
+		return intro;
+	}
