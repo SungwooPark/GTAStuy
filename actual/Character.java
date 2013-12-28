d6d9168bd794f61a79d35e225d0a048817348fbd
albertoh61 <albertodoh61@gmail.com>
2013-12-28 12:46:26 -0500

    updated

--- actual/Character.java
+++ actual/Character.java
@@ -12,10 +12,10 @@ public class Character {
 		
 
 	public String intro () {
-		String intro = "Welcome back " + name + ", we hope you enjoyed your summer"
-                intro = intro + ", but now it is time to get back to school."
-                intro = intro + " You have just crossed the Tribeca and face the haunting metal doors that are the entrance to Stuy."
-                intro = intro + " You enter and encounter a bright flash of light.........\n\n"
+		String intro = "Welcome back " + name + ", we hope you enjoyed your summer";
+                intro = intro + ", but now it is time to get back to school.";
+                intro = intro + " You have just crossed the Tribeca and face the haunting metal doors that are the entrance to Stuy.";
+                intro = intro + " You enter and encounter a bright flash of light........." + " \n \n";
                 intro = intro + "          || Welcome to GTAstuy ||";
 		return intro;
 	}
--- actual/Driver.java
+++ actual/Driver.java
@@ -4,5 +4,6 @@ import java.util.*;
 public class Driver{
     public static void main(String[] args){
         Game game = new Game();
+	
     }
 }
