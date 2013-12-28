464278397ef49a705ab4c96f03b6bc4ca61437dd
Sungwoo Park <iyw03211@gmail.com>
2013-12-28 13:11:27 -0500

    Added more stuff

--- actual/Game.java
+++ actual/Game.java
@@ -2,12 +2,11 @@ import java.util.*;
 import java.io.*;
 
 public class Game{
-    private Scanner scan;
     private String name;
     private Character player;
+    private Scanner scan = new Scanner(System.in);
 
     public Game(){
-        scan = new Scanner(System.in);
         System.out.println("Welcome to GTAStuy");
         System.out.println("Intro thingy");
 
@@ -16,5 +15,18 @@ public class Game{
 
         player = new Character(name);
         System.out.println(player.intro());
+        tutorial();
+    }
+
+    public void tutorial(){
+        System.out.println("You are late to your homeroom, but you left your lunch in your locker. What are you going to do?");
+        System.out.println("1. Go to locker 2. Go to homeroom 3. Check your status");
+        int choice = 0;
+        choice = scan.nextInt();
+        if (choice == 1){
+            System.out.println("Going to locker ... ");
+        }else{
+            System.out.println("Going to homeroom ... ");
+        }
     }
 }
